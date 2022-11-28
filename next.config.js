99f699f5bbbe36ba35f4982a2d1f5cde05377950
neal <neal0420yang@gmail.com>
2022-11-28 14:28:31 +0800

    final:

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     loader: "akamai",
     path: "",
   },
-  basePath: "/nextjs-pages",
-  assetPrefix: "/nextjs-pages",
+  basePath: "/AppWorkSchool-intro",
+  assetPrefix: "/AppWorkSchool-intro",
+  //會讓npm run dev 404
 };
 
 module.exports = nextConfig
